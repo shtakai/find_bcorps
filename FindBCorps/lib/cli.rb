22390658437b8a9efad0a2d7aec3c37ed384ae4d
sharonrieger <sharonarieger@gmail.com>
2018-11-23 09:07:07 -0500

    neatened up my code on these pages to be more concise

--- FindBCorps/lib/cli.rb
+++ FindBCorps/lib/cli.rb
@@ -2,4 +2,11 @@
 
 class CLI
 
+    def start 
+        greeting
+        scrape
+
+    end
+
+
 end
--- FindBCorps/lib/scraper.rb
+++ FindBCorps/lib/scraper.rb
@@ -1,9 +1,11 @@
 
 class Scraper
 
-  def self.scrape_data
+  def self.scrape
+    # store companies and their details here?. 
     corps_directory = []
-    
+
+    # scrape this web page and iterate over each of their details and store them in the corps_directory array
     doc = Nokogiri::HTML(open('https://bcorporation.net/directory'))
     doc.css('.node-company.card').each do |company_card|
      company_details = {
@@ -13,6 +15,7 @@ class Scraper
         offerings: company_card.css('field-name-field-products-and-services').text,
         location: company_card.css('field-name-field-country').text
       }
+      # push company_details hash into the corps_directory array.
       corps_directory << company_details
     end
   end
